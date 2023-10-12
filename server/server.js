69c61cc0bf95fefd221fd315561d9438920bf5d2
Park <parkerlamarbrook@gmail.com>
2023-10-11 21:39:15 -0600

    socket connection built

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
+const {instrument} = require('@socket.io/admin-ui');
+
 const io = require('socket.io')(3000, {
     cors: {
-        origin: ['http://localhost:8080']
+        origin: ['http://localhost:8080', 'https://admin.socket.io'],
     },
 });
 
 io.on('connection', socket => {
     console.log(socket.id);
-    socket.on('custom-event', (message) => {
-        console.log(message);
+    socket.on('send-message', (message, room) => {
+        if (room === "") {
+            socket.broadcast.emit('receive-message', message);
+        } else {
+            socket.to(room).emit('receive-message', message);
+        }
+    });
+    socket.on('join-room', (room, cb) => {
+        socket.join(room);
+        cb(`Joined ${room}`)
     });
 });
 
+instrument(io, { auth: false });
